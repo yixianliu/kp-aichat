1568717e9ac72759cc037a12d9ce10325d4079ab
hdxh0011 <zccem@163.com>
2025-08-05 10:40:48 +0800

    更新基本功能！

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,36 +1,24 @@
 <template>
-	<view class="container">
-		<view class="btn-area">
-			<navigator url="navigate/navigate?title=navigate" hover-class="navigator-hover">
-				<button type="default">跳转到新页面</button>
-			</navigator>
-			<br />
-			<navigator url="redirect/redirect?title=redirect" open-type="redirect" hover-class="other-navigator-hover">
-				<button type="default">在当前页打开</button>
-			</navigator>
-			<br />
-			<navigator url="/pages/index/login" open-type="redirect" hover-class="other-navigator-hover">
-				<button type="default">跳转tab页面</button>
-			</navigator>
-		</view>
-	</view>
+	<view class="container"></view>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {
-				href: "https://uniapp.dcloud.io/component/README?id=uniui",
-			};
+			return {};
 		},
-		methods: {},
+		onLoad() {
+			// 获取状态栏高度
+			this.statusBarHeight = uni.getSystemInfoSync()["statusBarHeight"];
+		},
+		methods: {
+			goBack() {
+				uni.navigateBack({
+					delta: 1,
+				});
+			},
+		},
 	};
 </script>
 
-<style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
-	}
-</style>
+<style></style>
